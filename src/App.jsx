6e042809741e542ffc42ff3557a2ae3f573fad6d
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2025-06-23 11:42:26 -0600

    Update App.jsx
    Fix the routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import { Routes,Route } from 'react-router-dom'
+import './App.css';
+import Navbar from './components/Navbar';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import CharacterList from './pages/CharacterList';
 
 function App() {
   return (
     <>
-    <NavBar/>
-    <div className='container'>
-      <Routes>
-        <Route path='/' element={<h1>List</h1>}></Route>
-        <Route path='/character/:id' element={<h1>Character</h1>}></Route>
-      </Routes>
-    </div>
+      <Navbar />
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<CharacterList />} />
+          <Route path='/character/:id' element={<h1>Character</h1>} />
+          <Route path='*' element={<Navigate to='/' />} />
+        </Routes>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
