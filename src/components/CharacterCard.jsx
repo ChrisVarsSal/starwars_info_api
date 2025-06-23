8fa85829f848699f0a6d851d5e7966f5f022f5ff
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2025-06-23 11:43:05 -0600

    Update CharacterCard.jsx
    Fix the details button

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,31 +1,26 @@
 import { Link } from "react-router-dom";
 
-function CharacterCard({ character }) {
-  const characterId = character.url.split("/").filter(Boolean).pop();
-  return (
-    <div className="card">
-      <div className="card-content">
-        <h3>{character.name}</h3>
-        <div className="card-info">
-          <span>Gender: </span>
-          {character.gender !== "n/a" ? character.gender : "No Available"}
+function CharacterCard({character}) {
+    const characterId=character.url.split('/').filter(Boolean).pop();
+    return (
+        <div className="card">
+            <div className="card-content">
+                <h3>{character.name}</h3>
+                <div className="card-info">
+                    <span>Gender: </span>{character.gender!=='n/a'?character.gender:'No Available'}
+                </div>
+                <div className="card-info">
+                    <span>Height: </span>{character.height!=='unknown'?character.height:'Unknown'}
+                </div>
+                 <div className="card-info">
+                    <span>Weight: </span>{character.weight!=='unknown'?character.weight:'Unknown'}
+                </div>
+                <div className="card-actions">
+                    <Link to={`/character/${characterId}`} className="btn">Detail</Link>
+                </div>
+            </div>
         </div>
-        <div className="card-info">
-          <span>Height: </span>
-          {character.height !== "unknown" ? character.height : "Unknown"}
-        </div>
-        <div className="card-info">
-          <span>Weight: </span>
-          {character.weight !== "unknown" ? character.weight : "Unknown"}
-        </div>
-        <div className="card-actions">
-          <Link to={`charater/${characterId}`} className="btn">
-            Details
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default CharacterCard;
