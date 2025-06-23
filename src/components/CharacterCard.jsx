0258aa8d2d822db8c698f1f6ea3e0348e14f6e74
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2025-06-23 11:23:50 -0600

    Create CharacterCard.jsx
    Creation of Character List

---
+++ src/components/CharacterCard.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+function CharacterCard({ character }) {
+  const characterId = character.url.split("/").filter(Boolean).pop();
+  return (
+    <div className="card">
+      <div className="card-content">
+        <h3>{character.name}</h3>
+        <div className="card-info">
+          <span>Gender: </span>
+          {character.gender !== "n/a" ? character.gender : "No Available"}
+        </div>
+        <div className="card-info">
+          <span>Height: </span>
+          {character.height !== "unknown" ? character.height : "Unknown"}
+        </div>
+        <div className="card-info">
+          <span>Weight: </span>
+          {character.weight !== "unknown" ? character.weight : "Unknown"}
+        </div>
+        <div className="card-actions">
+          <Link to={`charater/${characterId}`} className="btn">
+            Details
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CharacterCard;
