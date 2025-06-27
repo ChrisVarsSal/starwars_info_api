c4974878337e357bedfdf7e0000b0041bd5dd1d5
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2025-06-27 11:48:36 -0600

    Update App.jsx
    add the new routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar";
 import { Routes, Route, Navigate } from "react-router-dom";
 import CharacterList from "./pages/CharacterList";
 import CharacterDetail from "./pages/CharacterDetail";
+import PlanetList from "./pages/PlanetList";
+import PlanetDetail from "./pages/PlanetDetail";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<CharacterList />} />
           <Route path="/character/:id" element={<CharacterDetail />} />
+          <Route path="/planets" element={<PlanetList />} />
+          <Route path="/planet/:id" element={<PlanetDetail />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
