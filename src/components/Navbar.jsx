fe02c91585929e40e2986f6865c8713f78bd0874
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2025-06-27 14:53:22 -0600

    Update Navbar.jsx
    change the navgar for a bootstrap template

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,51 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="container navbar-container">
-        <Link to="/" className="navbar-logo">
-          <h1>Star Wars API</h1>
-        </Link>
-        <ul className="navbar-links">
-          <li>
-            <Link to="/">Characters</Link>
-          </li>
-          <li>
-            <Link to="/starships">Starships</Link>
-          </li>
-          <li>
-            <Link to="/planets">Planets</Link>
-          </li>
-        </ul>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <NavLink className="navbar-brand" to="/">
+          Star Wars Info
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                to="/"
+                end
+              >
+                Characters
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/planets">
+                Planets
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/starships">
+                Starships
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
