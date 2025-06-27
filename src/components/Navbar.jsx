33119a397701a12fa23760716210acedd02f2e5c
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2025-06-27 11:44:46 -0600

    Update Navbar.jsx
    add the new routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,24 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="container">
+      <div className="container navbar-container">
         <Link to="/" className="navbar-logo">
           <h1>Star Wars API</h1>
         </Link>
+        <ul className="navbar-links">
+          <li>
+            <Link to="/">Characters</Link>
+          </li>
+          <li>
+            <Link to="/ships">Ships</Link>
+          </li>
+          <li>
+            <Link to="/planets">Planets</Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
 }
+
 export default Navbar;
